3727677ecc032586d934b5772da7cd2a6a581a37
AapoX <Aapo.tuomainen@gmail.com>
2023-10-10 18:03:13 +0300

    game level base is done. No collisions yet

---
+++ player.py
@@ -0,0 +1,46 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((32, 64))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(topleft = pos)
+        
+        # player movement
+        self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
+        
+        # list that contains x and y values
+        self.direction = pygame.math.Vector2(0, 0)
+        
+        
+        
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_d]:
+            self.direction.x = 1
+        elif keys[pygame.K_a]:
+            self.direction.x = -1
+        else: 
+            self.direction.x = 0
+        
+        if keys[pygame.K_SPACE]:
+            self.jump()
+            
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y        
+        
+    def jump(self):
+        self.direction.y = self.jump_speed
+            
+        
+            
+    def update(self):
+        self.rect.x += self.direction.x * self.speed
+        self.get_input()
+        self.apply_gravity()
+        
