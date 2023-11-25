27aa1473007b0b6ab0afe01ee6c93d07232878bf
AapoX <Aapo.tuomainen@gmail.com>
2023-11-25 17:48:10 +0200

    deleted all temp files, replaced with working temp files LOL

--- assets/levels/tilesets/Terrain_test64x.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Terrain_test64x" tilewidth="64" tileheight="64" tilecount="247" columns="19">
- <image source="../../../../../../Downloads/Terrain_test64x.png" width="1216" height="832"/>
-</tileset>
